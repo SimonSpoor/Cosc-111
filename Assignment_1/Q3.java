904642adea6ae6c6a2436fbfca30f21a42d7afb0
SimonSpoor <sammoore359@gmail.com>
2022-09-06 17:49:11 -0700

    Create Q3.java

---
+++ Assignment_1/Q3.java
@@ -0,0 +1,11 @@
+public class Q3 {
+    public static void main(String[] args) {
+    //prints distance
+    double g = 9.81;
+    double t = 12; //listed t as a double incase it needs to be changed
+    double d = g * t / 2;
+    System.out.println(
+        "The distance travelled after " + t + " seconds is " + d
+    );
+    }
+}
