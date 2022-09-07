c421da46d99f04c2cd6e1d0067b36edbaf1be54b
SimonSpoor <sammoore359@gmail.com>
2022-09-06 17:49:09 -0700

    Create Q2.java

---
+++ Assignment_1/Q2.java
@@ -0,0 +1,15 @@
+
+public class Q2 {
+    public static void main(String[] args) {
+        //prints area and perimeter of rectangle
+        int rectangle_width = 7;
+        int rectangle_height = 9;
+        int rectangle_area = rectangle_height * rectangle_width;
+        int rectangle_perimeter = 2 * (rectangle_height + rectangle_width);
+        System.out.println(
+        "The area of a rectangle with a width of " + rectangle_width + " and a height of " +
+        rectangle_height + " is " + rectangle_area + " and the perimeter is " + rectangle_perimeter
+    );
+    }
+}
+
