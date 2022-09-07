9d60e69c06cef72510354241e6b6f82af72f6d8b
SimonSpoor <sammoore359@gmail.com>
2022-09-06 17:49:07 -0700

    Create Q1.java

---
+++ Assignment_1/Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args) {
+        //Prints UBC
+        System.out.println(
+            "U   U  B B     C C \n" +
+            "U   U  B   B  C    \n" +
+            "U   U  B B    C    \n" +
+            "U   U  B   B  C    \n" +
+            " U U   B B     C C "
+        );
+
+    }
+}
